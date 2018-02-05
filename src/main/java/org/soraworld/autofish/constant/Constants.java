d36e6af4f933cecd7025a3aeb15dd930d6b49bb7
Himmelt <master@void-3.cn>
2018-02-05 09:48:50 +0800

    1.0.1

--- src/main/java/org/soraworld/autofish/constant/Constants.java
+++ src/main/java/org/soraworld/autofish/constant/Constants.java
@@ -10,9 +10,9 @@ public final class Constants {
 
     public static final String MODID = "autofish";
     public static final String NAME = "AutoFishing";
-    public static final String VERSION = "1.7.10-1.0.0";
+    public static final String VERSION = "1.7.10-1.0.1";
     public static final String ACMCVERSION = "[1.7.10]";
 
-    public static final KeyBinding KEY_SWITCH = new KeyBinding("AutoFishing Switch", Keyboard.KEY_F, "key.categories.gameplay");
+    public static final KeyBinding KEY_SWITCH = new KeyBinding("AutoFishing Switch", Keyboard.KEY_H, "key.categories.gameplay");
 
 }
